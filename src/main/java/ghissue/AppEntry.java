1e442aeffde99b343b2725e261a3bd65f0c221f8
Gelin Luo <greenlaw110@gmail.com>
2018-04-15 07:37:56 +1000

    for GH625

--- src/main/java/ghissue/AppEntry.java
+++ src/main/java/ghissue/AppEntry.java
@@ -1,35 +1,25 @@
 package ghissue;
 
 import act.Act;
-import act.inject.DefaultValue;
-import act.util.Output;
 import org.osgl.mvc.annotation.GetAction;
+import org.osgl.mvc.annotation.PostAction;
+import org.osgl.util.S;
 
-/**
- * A simple hello world app entry
- *
- * Run this app, try to update some of the code, then
- * press F5 in the browser to watch the immediate change
- * in the browser!
- */
 @SuppressWarnings("unused")
 public class AppEntry {
 
-    /**
-     * The home (`/`) endpoint.
-     *
-     * This will accept a query parameter named `who` and
-     * render a template (resources/rythm/__package__/AppEntry/home.html),
-     * where `__package__` corresponding to the package name, e.g.
-     * if your package is `com.mycomp.myproj`, then `__package__`
-     * is `com/mycomp/myproj`.
-     *
-     * @param who
-     *      request query parameter to specify the hello target.
-     *      default value is `World`.
-     */
     @GetAction
-    public void home(@DefaultValue("World") @Output String who) {
+    public void home() {
+    }
+
+    @PostAction("{type}")
+    public String route1(String type) {
+        return type;
+    }
+
+    @PostAction("{type}/test")
+    public String route2(String type) {
+        return S.concat(type, "/list");
     }
 
     public static void main(String[] args) throws Exception {
